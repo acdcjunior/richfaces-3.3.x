fad865e93144fa59a12049ae397028f5f8a593c4
nbelaevski <nbelaevski@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-05-18 15:37:29 +0000

    Version changed to 3.3.2-SNAPSHOT

--- samples/createProject.sh
+++ samples/createProject.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn archetype:create -DarchetypeGroupId=org.richfaces.cdk -DarchetypeArtifactId=maven-archetype-jsfwebapp \
-         -DarchetypeVersion=3.3.1-SNAPSHOT -Dversion=3.3.1-SNAPSHOT -DgroupId=org.richfaces.samples -DartifactId=$1
+         -DarchetypeVersion=3.3.2-SNAPSHOT -Dversion=3.3.2-SNAPSHOT -DgroupId=org.richfaces.samples -DartifactId=$1
--- sandbox/ui/create.sh
+++ sandbox/ui/create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn archetype:create -DarchetypeGroupId=org.richfaces.cdk -DarchetypeArtifactId=maven-archetype-jsf-component -DarchetypeVersion=3.3.1-SNAPSHOT -DgroupId=org.richfaces.ui -DartifactId=${1}
+mvn archetype:create -DarchetypeGroupId=org.richfaces.cdk -DarchetypeArtifactId=maven-archetype-jsf-component -DarchetypeVersion=3.3.2-SNAPSHOT -DgroupId=org.richfaces.ui -DartifactId=${1}
