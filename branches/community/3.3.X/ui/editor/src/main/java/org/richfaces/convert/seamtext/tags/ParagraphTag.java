92e7b4017e212743badde04c95acde55a005bf08
Alex.Kolonitsky <Alex.Kolonitsky@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-05-22 13:46:58 +0000

    add copyright

--- branches/community/3.3.X/ui/editor/src/main/java/org/richfaces/convert/seamtext/tags/ParagraphTag.java
+++ branches/community/3.3.X/ui/editor/src/main/java/org/richfaces/convert/seamtext/tags/ParagraphTag.java
@@ -1,5 +1,31 @@
+/**
+ * License Agreement.
+ *
+ * Rich Faces - Natural Ajax for Java Server Faces (JSF)
+ *
+ * Copyright (C) 2007 Exadel, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
 package org.richfaces.convert.seamtext.tags;
 
+
+/**
+ * @author Alex.Kolonitsky
+ * @since 3.3.2.CR1
+ * */
 public class ParagraphTag extends LineTag {
 
     private static final long serialVersionUID = 1720000557944774249L;
--- branches/community/3.3.X/ui/editor/src/main/java/org/richfaces/convert/seamtext/tags/TagFactory.java
+++ branches/community/3.3.X/ui/editor/src/main/java/org/richfaces/convert/seamtext/tags/TagFactory.java
@@ -1,3 +1,24 @@
+/**
+ * License Agreement.
+ *
+ * Rich Faces - Natural Ajax for Java Server Faces (JSF)
+ *
+ * Copyright (C) 2007 Exadel, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
 package org.richfaces.convert.seamtext.tags;
 
 import static org.richfaces.convert.seamtext.tags.HtmlTag.*;
@@ -69,7 +90,6 @@ public class TagFactory {
                 }
             },
             
-//            getNewTagDefinition(PRE, SEAM_BACKTICK,    SEAM_BACKTICK),
             getNewTagDefinition(Q,   SEAM_DOUBLEQUOTE, SEAM_DOUBLEQUOTE),
             
             new LinkTag(),
