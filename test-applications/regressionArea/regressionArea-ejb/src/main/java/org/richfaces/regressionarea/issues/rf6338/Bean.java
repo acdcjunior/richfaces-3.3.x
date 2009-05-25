7a53422f2161404cd981d73b91ba36a57652bd40
mvitenkov <mvitenkov@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-05-25 16:36:55 +0000

    java bean for RF-6338 added

---
+++ test-applications/regressionArea/regressionArea-ejb/src/main/java/org/richfaces/regressionarea/issues/rf6338/Bean.java
@@ -0,0 +1,16 @@
+package org.richfaces.regressionarea.issues.rf6338;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("rf6338")
+@Scope(ScopeType.SESSION)
+public class Bean {
+
+	public Bean() {		
+	}
+	
+	public void action() {	
+	}
+}
