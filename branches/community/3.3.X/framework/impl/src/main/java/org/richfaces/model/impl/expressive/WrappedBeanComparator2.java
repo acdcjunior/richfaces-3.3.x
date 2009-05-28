e602ca5d92d0224ad09d8f9c4bb87c7023c48a3d
nbelaevski <nbelaevski@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-05-28 16:32:10 +0000

    https://jira.jboss.org/jira/browse/RF-7268

--- branches/community/3.3.X/framework/impl/src/main/java/org/richfaces/model/impl/expressive/WrappedBeanComparator2.java
+++ branches/community/3.3.X/framework/impl/src/main/java/org/richfaces/model/impl/expressive/WrappedBeanComparator2.java
@@ -101,9 +101,9 @@ public final class WrappedBeanComparator2 implements Comparator<Object> {
 				} else if (p1 instanceof Comparable && p2 instanceof Comparable) {
 					result = ((Comparable<Object>) p1).compareTo(p2);
 				} else if (p1 == null && p2 != null) {
-					result = 1;
-				} else if (p2 == null && p1 != null) {
 					result = -1;
+				} else if (p2 == null && p1 != null) {
+					result = 1;
 				}
 				
 				if (ordering.equals(Ordering.DESCENDING)) {
