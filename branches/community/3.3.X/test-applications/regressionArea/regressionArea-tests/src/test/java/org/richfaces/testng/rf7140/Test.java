3b613f6895a601e5c0e0ff06515f6aed4287e1ed
mvitenkov <mvitenkov@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-06-01 09:22:03 +0000

    test correction

--- branches/community/3.3.X/test-applications/regressionArea/regressionArea-tests/src/test/java/org/richfaces/testng/rf7140/Test.java
+++ branches/community/3.3.X/test-applications/regressionArea/regressionArea-tests/src/test/java/org/richfaces/testng/rf7140/Test.java
@@ -4,26 +4,27 @@ import org.richfaces.SeleniumTestBase;
 import org.testng.Assert;
 
 public class Test extends SeleniumTestBase {
+	private static final String FORM = "form";
 
 	@org.testng.annotations.Test
 	public void testExecute() throws Exception {
 		renderPage();
 		
-		clickAjaxCommandAndWait("//form[@id='form']/table[1]/thead/tr/th");
-		String t1FirstRow = selenium.getTable("//form[@id='form']/table[1].1.0");
-		String t1LastRow = selenium.getTable("//form[@id='form']/table[1].4.0");
+		clickAjaxCommandAndWait("//form[@id='" + FORM + "']/table[1]/thead/tr/th");
+		String t1FirstRow = selenium.getTable("//form[@id='" + FORM + "']/table[1].1.0");
+		String t1LastRow = selenium.getTable("//form[@id='" + FORM + "']/table[1].4.0");
 		
 		clickAjaxCommandAndWait("//form[@id='form']/table[1]/thead/tr/th");
-		Assert.assertEquals(selenium.getTable("//form[@id='form']/table[1].1.0"), t1LastRow);
-		Assert.assertEquals(selenium.getTable("//form[@id='form']/table[1].4.0"), t1FirstRow);
+		Assert.assertEquals(selenium.getTable("//form[@id='" + FORM + "']/table[1].1.0"), t1LastRow);
+		Assert.assertEquals(selenium.getTable("//form[@id='" + FORM + "']/table[1].4.0"), t1FirstRow);
 		
 		clickAjaxCommandAndWait("//form[@id='form']/table[4]/thead/tr/th");
-		String t4FirstRow = selenium.getTable("//form[@id='form']/table[4].1.0");
-		String t4LastRow = selenium.getTable("//form[@id='form']/table[4].4.0");		
+		String t4FirstRow = selenium.getTable("//form[@id='" + FORM + "']/table[4].1.0");
+		String t4LastRow = selenium.getTable("//form[@id='" + FORM + "']/table[4].4.0");		
 		
 		clickAjaxCommandAndWait("//form[@id='form']/table[4]/thead/tr/th");
-		Assert.assertEquals(selenium.getTable("//form[@id='form']/table[4].1.0"), t4LastRow);
-		Assert.assertEquals(selenium.getTable("//form[@id='form']/table[4].4.0"), t4FirstRow);
+		Assert.assertEquals(selenium.getTable("//form[@id='" + FORM + "']/table[4].1.0"), t4LastRow);
+		Assert.assertEquals(selenium.getTable("//form[@id='" + FORM + "']/table[4].4.0"), t4FirstRow);
 		
 	}
 
