013d5f45bd4160b89a0cbe13dd476a9530ce7427
artdaw <artdaw@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-07-01 13:39:37 +0000

    https://jira.jboss.org/jira/browse/RF-7340 - a4j:support is reviewed

---
+++ test-applications/richfaces-docs/src/main/java/org/docs/richfaces/MediaData.java
@@ -0,0 +1,48 @@
+package org.docs.richfaces;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.io.Serializable;
+
+public class MediaData implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    Integer Width=110;
+    Integer Height=50;
+    Color Background=new Color(190, 214, 248);
+    Color DrawColor=new Color(0,0,0);
+    Font font = new Font("Serif", Font.TRUETYPE_FONT, 30);
+    
+    public Font getFont() {
+		return font;
+	}
+	public void setFont(Font font) {
+		this.font = font;
+	}
+	public MediaData() {
+    }
+    public Color getBackground() {
+        return Background;
+    }
+    public void setBackground(Color background) {
+        Background = background;
+    }
+    public Color getDrawColor() {
+        return DrawColor;
+    }
+    public void setDrawColor(Color drawColor) {
+        DrawColor = drawColor;
+    }
+    public Integer getHeight() {
+        return Height;
+    }
+    public void setHeight(Integer height) {
+        Height = height;
+    }
+    public Integer getWidth() {
+        return Width;
+    }
+    public void setWidth(Integer width) {
+        Width = width;
+    }
+}
---
+++ test-applications/richfaces-docs/src/main/java/org/docs/richfaces/Support.java
@@ -0,0 +1,23 @@
+package org.docs.richfaces;
+
+import java.io.File;
+
+public class Support {
+	String text = new String();
+	String items = new String();
+	public String getItems() {
+		return items;
+	}
+
+	public void setItems(String items) {
+		this.items = items;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
