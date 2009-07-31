1a48f19f3e22c6dc2ef20effcaed8714be8cafa7
konstantin.mishin <konstantin.mishin@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-07-31 16:12:54 +0000

    RF-7627

--- branches/community/3.3.X/ui/colorPicker/src/main/java/org/richfaces/renderkit/ColorPickerRendererBase.java
+++ branches/community/3.3.X/ui/colorPicker/src/main/java/org/richfaces/renderkit/ColorPickerRendererBase.java
@@ -1,18 +1,26 @@
 package org.richfaces.renderkit;
 
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 
 import org.ajax4jsf.context.AjaxContext;
 import org.ajax4jsf.javascript.JSFunctionDefinition;
 import org.ajax4jsf.javascript.ScriptUtils;
+import org.ajax4jsf.renderkit.ComponentVariables;
+import org.ajax4jsf.renderkit.ComponentsVariableResolver;
 import org.richfaces.component.UIColorPicker;
 import org.richfaces.component.util.MessageUtil;
 
 public abstract class ColorPickerRendererBase  extends InputRendererBase {
+
+	public static final String BUNDLE = ColorPickerRendererBase.class.getPackage().getName() + ".colorPicker";
 
 	public void addPopupToAjaxRendered(FacesContext context, UIColorPicker component) {
 		AjaxContext ajaxContext = AjaxContext.getCurrentInstance(context);
@@ -60,5 +68,64 @@ public abstract class ColorPickerRendererBase  extends InputRendererBase {
 
 		throw new IllegalArgumentException("Illegal color value: [" + value + "] for component " + 
 			MessageUtil.getLabel(context, component));
+	}
+	
+	public void initButtonLabels(FacesContext context, UIComponent component){
+		ComponentVariables variables = ComponentsVariableResolver.getVariables(this, component);
+		Locale locale = getLocale(context);
+		ClassLoader loader = getClassLoader();
+		ResourceBundle bundleApplication = null;
+		ResourceBundle bundleExternal = null;
+		String messageBundle = context.getApplication().getMessageBundle();
+		if (messageBundle != null) {
+			bundleApplication = ResourceBundle.getBundle(messageBundle, locale,
+					loader);
+		}
+		try {
+			bundleExternal = ResourceBundle.getBundle(BUNDLE, locale, loader);
+		} catch (MissingResourceException e) {}
+		String[] names = {"Apply", "Cancel"};
+		for (String name : names) {
+			String label = null;
+		    String bundleKey = "RICH_COLORPICKER_" + name.toUpperCase() + "_LABEL";
+		    if (bundleApplication != null) {
+		    	try {
+		    		label = bundleApplication.getString(bundleKey);
+		    	} catch (MissingResourceException mre) {
+			    // Current key was not found, ignore this exception;
+		    	}
+		    }
+		    if (label == null && bundleExternal != null) {
+		    	try {
+		    		label = bundleExternal.getString(bundleKey);
+		    	} catch (MissingResourceException mre) {
+			    // Current key was not found, ignore this exception;
+		    	}
+		    }
+		    if (label == null) {
+		    	label = name;
+		    }
+		    variables.setVariable(name, label);
+		}
+	}
+	
+	private static Locale getLocale(FacesContext context) {
+		Locale locale = null;
+		UIViewRoot viewRoot = context.getViewRoot();
+		if (viewRoot != null) {
+			locale = viewRoot.getLocale();
+		}
+		if (locale == null) {
+			locale = Locale.getDefault();
+		}
+		return locale;
+	}
+	
+	private static ClassLoader getClassLoader() {
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		if (loader == null) {
+			loader = ClassLoader.getSystemClassLoader();
+		}
+		return loader;
 	}
 }
