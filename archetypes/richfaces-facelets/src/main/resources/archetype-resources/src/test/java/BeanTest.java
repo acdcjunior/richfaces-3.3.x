82e88044c2f85772dd2b6c1cb209bbb3e49599f5
prabhat.jha <prabhat.jha@jboss.com>
2009-08-27 15:43:07 +0000

    archetype for richfaces and facelet project

---
+++ archetypes/richfaces-facelets/src/main/resources/archetype-resources/src/test/java/BeanTest.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import junit.framework.TestCase;
+
+public class BeanTest extends TestCase {
+	public BeanTest(String testName) {
+		super(testName);
+	}
+
+	public void testStub() throws Exception {
+
+	}
+}
