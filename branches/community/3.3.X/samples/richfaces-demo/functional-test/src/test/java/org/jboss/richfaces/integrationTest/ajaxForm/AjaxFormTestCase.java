eee2eaa7df22b221ff033c5fe9b050fcd9fd4c5f
lfryc@redhat.com <lfryc@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-09-01 19:13:48 +0000

    - refactored Ajax Form test cases to follow conventions

--- branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/ajaxForm/AjaxFormTestCase.java
+++ branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/ajaxForm/AjaxFormTestCase.java
@@ -1,7 +1,28 @@
+/**
+ * License Agreement.
+ *
+ *  JBoss RichFaces
+ *
+ * Copyright (C) 2009  Red Hat, Inc.
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this test suite; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 package org.jboss.richfaces.integrationTest.ajaxForm;
 
 import org.jboss.richfaces.integrationTest.AbstractSeleniumRichfacesTestCase;
-import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.thoughtworks.selenium.Wait;
@@ -11,37 +32,39 @@ import com.thoughtworks.selenium.Wait;
  * @version $Revision$
  */
 public class AjaxFormTestCase extends AbstractSeleniumRichfacesTestCase {
+
+	private String LOC_LEGEND_HEADER = getLoc("LEGEND_HEADER");
+	private String LOC_BUTTON_AJAX = getLoc("BUTTON_AJAX");
+	private String LOC_BUTTON_NON_AJAX = getLoc("BUTTON_NON_AJAX");
+	private String LOC_OUTPUT_TEXT_RELATIVE = getLoc("OUTPUT_TEXT_RELATIVE");
+	private String LOC_OUTPUT_AJAX = format(LOC_OUTPUT_TEXT_RELATIVE, LOC_BUTTON_AJAX);
+	private String LOC_OUTPUT_NON_AJAX = format(LOC_OUTPUT_TEXT_RELATIVE, LOC_BUTTON_NON_AJAX);
+
+	private final String MSG_OUTPUT_NON_AJAX = getMsg("OUTPUT_NON_AJAX");
+	private final String MSG_OUTPUT_AJAX = getMsg("OUTPUT_AJAX");
+
 	/**
-	 * Opens specified page
+	 * Sends non-Ajax request and check that it changes both of output fields.
 	 */
-	private void openPage() {
-		selenium.open(contextPath + "/richfaces/form.jsf?c=form&tab=usage");
-
-		scrollIntoView(header, true);
-	}
-
-	private String header = "//legend[text()='AjaxForm Demo']";
-	private String ajaxButton = getLoc("ajax-form--ajax-button");
-	private String nonAjaxButton = getLoc("ajax-form--non-ajax-button");
-	private String textRelative = "ajax-form--text-relative";
-	private String ajaxText = formatLoc(textRelative, ajaxButton);
-	private String nonAjaxText = formatLoc(textRelative, nonAjaxButton);
-
 	@Test
 	public void testNonAjaxSubmit() {
-		openPage();
 		nonAjaxSubmit();
 	}
 
+	/**
+	 * Sends Ajax request and check that it changes only ajax-rendered field.
+	 */
 	@Test
 	public void testAjaxSubmit() {
-		openPage();
 		ajaxSubmit();
 	}
 
+	/**
+	 * Test interleaving of non-Ajax and Ajax requested changes and checks that
+	 * it is rendered in right way.
+	 */
 	@Test
 	public void testInterleaving() {
-		openPage();
 		ajaxSubmit();
 		nonAjaxSubmit();
 		ajaxSubmit();
@@ -49,28 +72,39 @@ public class AjaxFormTestCase extends AbstractSeleniumRichfacesTestCase {
 	}
 
 	public void nonAjaxSubmit() {
-		String expected = getMess("ajax-form--non-ajax-result");
-
-		selenium.click(nonAjaxButton);
+		selenium.click(LOC_BUTTON_NON_AJAX);
 		selenium.waitForPageToLoad(String.valueOf(Wait.DEFAULT_TIMEOUT));
-		scrollIntoView(header, true);
 
-		String actual = selenium.getText(ajaxText);
-		Assert.assertEquals(actual, expected);
+		scrollIntoView(LOC_LEGEND_HEADER, true);
 
-		actual = selenium.getText(nonAjaxText);
-		Assert.assertEquals(actual, expected);
+		// both of outputs should be same
+		String actual = selenium.getText(LOC_OUTPUT_AJAX);
+		assertEquals(actual, MSG_OUTPUT_NON_AJAX);
+
+		actual = selenium.getText(LOC_OUTPUT_NON_AJAX);
+		assertEquals(actual, MSG_OUTPUT_NON_AJAX);
 	}
 
 	public void ajaxSubmit() {
-		String expected = getMess("ajax-form--ajax-result");
+		String startingNonAjaxText = selenium.getText(LOC_OUTPUT_NON_AJAX);
 
-		String startingNonAjaxText = selenium.getText(nonAjaxText);
+		// ajax-rendered output should be set right
+		selenium.click(LOC_BUTTON_AJAX);
+		waitForTextEquals(LOC_OUTPUT_AJAX, MSG_OUTPUT_AJAX);
 
-		selenium.click(ajaxButton);
-		waitForTextEquals(ajaxText, expected);
+		// non-ajax output should stay same like before the request
+		String actual = selenium.getText(LOC_OUTPUT_NON_AJAX);
+		assertEquals(actual, startingNonAjaxText);
+	}
 
-		String actual = selenium.getText(nonAjaxText);
-		Assert.assertEquals(actual, startingNonAjaxText);
+	/**
+	 * Opens specific component's page
+	 */
+	@SuppressWarnings("unused")
+	@BeforeMethod
+	private void loadPage() {
+		openComponent("Ajax Form");
+
+		scrollIntoView(LOC_LEGEND_HEADER, true);
 	}
 }
