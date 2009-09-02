6bccc1007fdcf25db91373ca52b1b8a158653075
lfryc@redhat.com <lfryc@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-09-02 15:01:21 +0000

    - refactored Style test cases to follow conventions

--- branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/style/StyleTestCase.java
+++ branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/style/StyleTestCase.java
@@ -1,12 +1,33 @@
+/**
+ * License Agreement.
+ *
+ *  JBoss RichFaces
+ *
+ * Copyright (C) 2009  Red Hat, Inc.
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this test suite; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 package org.jboss.richfaces.integrationTest.style;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import junit.framework.Assert;
+import static org.testng.Assert.*;
 
 import org.jboss.richfaces.integrationTest.AbstractSeleniumRichfacesTestCase;
 import org.jboss.test.selenium.waiting.Wait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -14,44 +35,50 @@ import org.testng.annotations.Test;
  * @version $Revision$
  */
 public class StyleTestCase extends AbstractSeleniumRichfacesTestCase {
-	/**
-	 * Opens specified page
-	 */
-	private void openPage() {
-		selenium.open(contextPath
-				+ "/richfaces/style.jsf?c=loadStyle&tab=usage");
-		scrollIntoView(header, true);
-	}
 
-	private String header = getLoc("style--header");
-	private String linkDeepMarine = getLoc("style--link--deep-marine");
-	private String linkBlueSky = getLoc("style--link--blue-sky");
-	private String linkJapanCherry = getLoc("style--link--japan-cherry");
-	private String tableOutput = getLoc("style--table--output");
-	private String colorDeepMarine = getMess("style--color--deep-marine");
-	private String colorBlueSky = getMess("style--color--blue-sky");
-	private String colorJapanCherry = getMess("style--color--japan-cherry");
+	private String LOC_FIELDSET_HEADER = getLoc("FIELDSET_HEADER");
+	private String LOC_LINK_DEEP_MARINE = getLoc("LINK_DEEP_MARINE");
+	private String LOC_LINK_BLUE_SKY = getLoc("LINK_BLUE_SKY");
+	private String LOC_LINK_JAPAN_CHERRY = getLoc("LINK_JAPAN_CHERRY");
+	private String LOC_PANEL = getLoc("PANEL");
+
+	private String MSG_COLOR_DEEP_MARINE = getMsg("COLOR_DEEP_MARINE");
+	private String MSG_COLOR_BLUE_SKY = getMsg("COLOR_BLUE_SKY");
+	private String MSG_COLOR_JAPAN_CHERRY = getMsg("COLOR_JAPAN_CHERRY");
 
 	Map<String, String> relation = new LinkedHashMap<String, String>() {
+		private static final long serialVersionUID = 1576758688687867261L;
 		{
-			put(linkDeepMarine, colorDeepMarine);
-			put(linkBlueSky, colorBlueSky);
-			put(linkJapanCherry, colorJapanCherry);
+			put(LOC_LINK_DEEP_MARINE, MSG_COLOR_DEEP_MARINE);
+			put(LOC_LINK_BLUE_SKY, MSG_COLOR_BLUE_SKY);
+			put(LOC_LINK_JAPAN_CHERRY, MSG_COLOR_JAPAN_CHERRY);
 		}
 	};
 
+	/**
+	 * Clicks on the skin names and waits for background-color changes
+	 * appropriately.
+	 */
 	@Test
 	public void testBackgroundColor() {
-		openPage();
+		for (final String locLink : relation.keySet()) {
+			final String msgColor = relation.get(locLink);
 
-		for (final String link : relation.keySet()) {
-			final String color = relation.get(link);
-
-			selenium.click(link);
+			selenium.click(locLink);
 			selenium.waitForPageToLoad(Long.toString(Wait.DEFAULT_TIMEOUT));
-			scrollIntoView(header, true);
 
-			Assert.assertEquals(color, getStyle(tableOutput, "background-color"));
+			scrollIntoView(LOC_FIELDSET_HEADER, true);
+
+			assertEquals(msgColor, getStyle(LOC_PANEL, "background-color"), format(
+					"background-color for '{0}' skin does not match", selenium.getText(locLink)));
 		}
+	}
+
+	@SuppressWarnings("unused")
+	@BeforeMethod
+	private void loadPage() {
+		openComponent("Style");
+
+		scrollIntoView(LOC_FIELDSET_HEADER, true);
 	}
 }
