c51f4e663385cd233095d9d50e8bb1004cb1ec4c
lfryc <lfryc@redhat.com>
2009-09-03 12:19:04 +0000

    - refactored Graph Validator test cases to follow conventions

--- samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/graphValidator/GraphValidatorTestCase.java
+++ samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/graphValidator/GraphValidatorTestCase.java
@@ -1,6 +1,27 @@
+/**
+ * License Agreement.
+ *
+ *  JBoss RichFaces
+ *
+ * Copyright (C) 2009  Red Hat, Inc.
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this test suite; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 package org.jboss.richfaces.integrationTest.graphValidator;
 
 import org.jboss.richfaces.integrationTest.AbstractSeleniumRichfacesTestCase;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -8,144 +29,208 @@ import org.testng.annotations.Test;
  * @version $Revision$
  */
 public class GraphValidatorTestCase extends AbstractSeleniumRichfacesTestCase {
+
+	private String LOC_FIELDSET_HEADER_USEF_INFO = getLoc("FIELDSET_HEADER_USEF_INFO");
+	private String LOC_BUTTON_SUBMIT_USER_INFO = getLoc("BUTTON_SUBMIT_USER_INFO");
+	private String LOC_VALIDATION_MESSAGE_RELATIVE = getLoc("VALIDATION_MESSAGE_RELATIVE");
+	private String LOC_INPUT_NAME = getLoc("INPUT_NAME");
+	private String LOC_INPUT_AGE = getLoc("INPUT_AGE");
+	private String LOC_INPUT_EMAIL = getLoc("INPUT_EMAIL");
+
+	private final String MSG_INPUT_VALUE_IS_LESS_THAN_MINIMUM = getMsg("INPUT_VALUE_IS_LESS_THAN_MINIMUM");
+	private final String MSG_INPUT_VALUE_IS_GREATER_THAN_MAXIMUM = getMsg("INPUT_VALUE_IS_GREATER_THAN_MAXIMUM");
+	private final String MSG_INPUT_VALID_NAME = getMsg("INPUT_VALID_NAME");
+	private final String MSG_INPUT_MUST_BE_GREATER_THAN_OR_EQUAL = getMsg("INPUT_MUST_BE_GREATER_THAN_OR_EQUAL");
+	private final String MSG_INPUT_MUST_BE_LESS_THAN_OR_EQUAL = getMsg("INPUT_MUST_BE_LESS_THAN_OR_EQUAL");
+	private final String MSG_INPUT_IS_NOT_NUMBER = getMsg("INPUT_IS_NOT_NUMBER");
+	private final String MSG_INPUT_VALID_AGE = getMsg("INPUT_VALID_AGE");
+	private final String MSG_INPUT_NOT_WELL_FORMED_EMAIL_1 = getMsg("INPUT_NOT_WELL_FORMED_EMAIL_1");
+	private final String MSG_INPUT_NOT_WELL_FORMED_EMAIL_2 = getMsg("INPUT_NOT_WELL_FORMED_EMAIL_2");
+	private final String MSG_INPUT_WELL_FORMED_EMAIL = getMsg("INPUT_WELL_FORMED_EMAIL");
+	private final String MSG_OUTPUT_LENGTH_MUST_BE_BETWEEN = getMsg("OUTPUT_LENGTH_MUST_BE_BETWEEN");
+	private final String MSG_OUTPUT_STRING_CONTAIN_ONLY_SPACES = getMsg("OUTPUT_STRING_CONTAIN_ONLY_SPACES");
+	private final String MSG_OUTPUT_MAY_NOT_BE_NULL = getMsg("OUTPUT_MAY_NOT_BE_NULL");
+	private final String MSG_OUTPUT_MUST_BE_GREATER_THAN_OR_EQUAL = getMsg("OUTPUT_MUST_BE_GREATER_THAN_OR_EQUAL");
+	private final String MSG_OUTPUT_MUST_BE_LESS_THAN_OR_EQUAL = getMsg("OUTPUT_MUST_BE_LESS_THAN_OR_EQUAL");
+	private final String MSG_OUTPUT_MUST_BE_A_NUMBER = getMsg("OUTPUT_MUST_BE_A_NUMBER");
+	private final String MSG_OUTPUT_MAY_NOT_BE_NULL_OR_EMPTY = getMsg("OUTPUT_MAY_NOT_BE_NULL_OR_EMPTY");
+	private final String MSG_OUTPUT_NOT_WELL_FORMED_EMAIL = getMsg("OUTPUT_NOT_WELL_FORMED_EMAIL");
+
 	/**
-	 * Opens specified page
+	 * Try type no chars in input name and checks that value required message
+	 * will appear.
 	 */
-	public void openPage() {
-		selenium.open(contextPath
-				+ "/richfaces/graphValidator.jsf?c=graphValidator&tab=usage");
-		scrollIntoView(header, true);
+	@Test
+	public void testNameValueRequired() {
+		final String validationMessage = format(MSG_OUTPUT_MAY_NOT_BE_NULL_OR_EMPTY, LOC_INPUT_NAME);
+		typeAndSubmit(LOC_INPUT_NAME, "");
+		waitForTextEquals(getMessageFor(LOC_INPUT_NAME), validationMessage);
 	}
 
-	private String header = getLoc("graph-validator--header1");
-	private String buttonSubmit = getLoc("graph-validator--button--submit1");
-	private String inputName = getLoc("graph-validator--input--name");
-	private String inputEmail = getLoc("graph-validator--input--email");
-	private String inputAge = getLoc("graph-validator--input--age");
-	private String relativeValidationMessage = getLoc("graph-validator--relative--validation-message");
+	/**
+	 * Try type string of length less than minimum length in name input and
+	 * checks that validation message will appear.
+	 */
+	@Test
+	public void testNameMinimumLength() {
+		typeAndSubmit(LOC_INPUT_NAME, MSG_INPUT_VALUE_IS_LESS_THAN_MINIMUM);
+		waitForTextEquals(getMessageFor(LOC_INPUT_NAME), MSG_OUTPUT_LENGTH_MUST_BE_BETWEEN);
+	}
+
+	/**
+	 * Try type string of length greater than maximum length in name input and
+	 * checks that validation message will appear.
+	 */
+	@Test
+	public void testNameMaximumLength() {
+		typeAndSubmit(LOC_INPUT_NAME, MSG_INPUT_VALUE_IS_GREATER_THAN_MAXIMUM);
+		waitForTextEquals(getMessageFor(LOC_INPUT_NAME), MSG_OUTPUT_LENGTH_MUST_BE_BETWEEN);
+	}
+
+	/**
+	 * Enter only one space to name input and checks that validation message
+	 * will appear.
+	 */
+	@Test
+	public void testNameOnlySpacesPattern() {
+		typeAndSubmit(LOC_INPUT_NAME, "  ");
+		waitForTextEquals(getMessageFor(LOC_INPUT_NAME), MSG_OUTPUT_STRING_CONTAIN_ONLY_SPACES);
+	}
+
+	/**
+	 * Try type the invalid name input and checks that after typing valid input
+	 * will validation message disappear.
+	 */
+	@Test
+	public void testNameMessageDisappers() {
+		// first violate validation error
+		testNameValueRequired();
+		// then try valid input
+		typeAndSubmit(LOC_INPUT_NAME, MSG_INPUT_VALID_NAME);
+		waitForTextEquals(getMessageFor(LOC_INPUT_NAME), "");
+	}
+
+	/**
+	 * Try type no chars in age input and checks that the validation message
+	 * value required will appear.
+	 */
+	@Test
+	public void testAgeValueRequired() {
+		typeAndSubmit(LOC_INPUT_AGE, "");
+		waitForTextEquals(getMessageFor(LOC_INPUT_AGE), MSG_OUTPUT_MAY_NOT_BE_NULL);
+	}
+
+	/**
+	 * Try input age less than minimum and checks that the validation message
+	 * will appear.
+	 */
+	@Test
+	public void testAgeMinimumValue() {
+		typeAndSubmit(LOC_INPUT_AGE, MSG_INPUT_MUST_BE_LESS_THAN_OR_EQUAL);
+		waitForTextEquals(getMessageFor(LOC_INPUT_AGE), MSG_OUTPUT_MUST_BE_LESS_THAN_OR_EQUAL);
+	}
+
+	/**
+	 * Try input age greater than maximum and checks that the validation message
+	 * will appear.
+	 */
+	@Test
+	public void testAgeMaximumValue() {
+		typeAndSubmit(LOC_INPUT_AGE, MSG_INPUT_MUST_BE_GREATER_THAN_OR_EQUAL);
+		waitForTextEquals(getMessageFor(LOC_INPUT_AGE), MSG_OUTPUT_MUST_BE_GREATER_THAN_OR_EQUAL);
+	}
+
+	/**
+	 * Try input age as non-Integer (alphabetical chars) and checks that
+	 * validation message will appear.
+	 */
+	@Test
+	public void testAgeIntegerOnly() {
+		final String validationMessage = format(MSG_OUTPUT_MUST_BE_A_NUMBER, LOC_INPUT_AGE);
+		typeAndSubmit(LOC_INPUT_AGE, MSG_INPUT_IS_NOT_NUMBER);
+		waitForTextEquals(getMessageFor(LOC_INPUT_AGE), validationMessage);
+	}
+
+	/**
+	 * Try input no input first and wait for validation message appears and then
+	 * type a valid input and checks that validation message will disappear.
+	 */
+	@Test
+	public void testAgeMessageDisappers() {
+		// first violate validation error
+		testAgeValueRequired();
+		// then try valid input
+		typeAndSubmit(LOC_INPUT_AGE, MSG_INPUT_VALID_AGE);
+		waitForTextEquals(getMessageFor(LOC_INPUT_AGE), "");
+	}
+
+	/**
+	 * Try to enter empty email address and checks that validation message
+	 * appears.
+	 */
+	@Test
+	public void testEmailMayNotBeNullOrEmpty() {
+		typeAndSubmit(LOC_INPUT_EMAIL, "");
+		waitForTextEquals(getMessageFor(LOC_INPUT_EMAIL), MSG_OUTPUT_MAY_NOT_BE_NULL_OR_EMPTY);
+	}
+
+	/**
+	 * Try to enter one space like a email address and checks that validation
+	 * message appears.
+	 */
+	@Test
+	public void testEmailOnlySpaces() {
+		typeAndSubmit(LOC_INPUT_EMAIL, " ");
+		waitForTextEquals(getMessageFor(LOC_INPUT_EMAIL), MSG_OUTPUT_NOT_WELL_FORMED_EMAIL);
+	}
+
+	/**
+	 * Enter bad email address (only domain name suffix) and checks that
+	 * validation message appears.
+	 */
+	@Test
+	public void testEmailBad1() {
+		typeAndSubmit(LOC_INPUT_EMAIL, MSG_INPUT_NOT_WELL_FORMED_EMAIL_1);
+		waitForTextEquals(getMessageFor(LOC_INPUT_EMAIL), MSG_OUTPUT_NOT_WELL_FORMED_EMAIL);
+	}
+
+	/**
+	 * Enter bad email address (only @ char with domain name suffix) and checks
+	 * that testValidation message appears.
+	 */
+	@Test
+	public void testEmailBad2() {
+		typeAndSubmit(LOC_INPUT_EMAIL, MSG_INPUT_NOT_WELL_FORMED_EMAIL_2);
+		waitForTextEquals(getMessageFor(LOC_INPUT_EMAIL), MSG_OUTPUT_NOT_WELL_FORMED_EMAIL);
+	}
+
+	/**
+	 * Enter bad email address, wait for validation message appear and then try
+	 * to enter valid email address and checks that validation message appear.
+	 */
+	@Test
+	public void testEmailMessageDisappers() {
+		// first violate validation error
+		testEmailMayNotBeNullOrEmpty();
+		// then try valid input
+		typeAndSubmit(LOC_INPUT_EMAIL, MSG_INPUT_WELL_FORMED_EMAIL);
+		waitForTextEquals(getMessageFor(LOC_INPUT_EMAIL), "");
+	}
 
 	private void typeAndSubmit(String locator, String value) {
 		selenium.type(locator, value);
-		selenium.click(buttonSubmit);
+		selenium.click(LOC_BUTTON_SUBMIT_USER_INFO);
 	}
 
-	public String getMessageFor(String locator) {
-		return format(relativeValidationMessage, locator);
+	private String getMessageFor(String locator) {
+		return format(LOC_VALIDATION_MESSAGE_RELATIVE, locator);
 	}
 
-	@Test
-	public void nameValueRequired() {
-		openPage();
-		typeAndSubmit(inputName, "");
-		waitForTextEquals(getMessageFor(inputName),
-				getMess("may-not-be-null-or-empty"));
-	}
+	@SuppressWarnings("unused")
+	@BeforeMethod
+	private void loadPage() {
+		openComponent("Graph Validator");
 
-	@Test
-	public void nameMinimumLength() {
-		openPage();
-		typeAndSubmit(inputName, getMess("less-than-minimum--enter"));
-		waitForTextEquals(getMessageFor(inputName),
-				getMess("bean-name--length"));
-	}
-
-	@Test
-	public void nameMaximumLength() {
-		openPage();
-		typeAndSubmit(inputName, getMess("greater-than-maximum--enter"));
-		waitForTextEquals(getMessageFor(inputName),
-				getMess("bean-name--length"));
-	}
-
-	@Test
-	public void nameOnlySpacesPattern() {
-		openPage();
-		typeAndSubmit(inputName, " ");
-		waitForTextEquals(getMessageFor(inputName), formatMess(
-				"less-than-minimum-of-2", selenium.getAttribute(format(
-						"{0}/@id", inputName))));
-	}
-
-	@Test
-	public void nameMessageDisappers() {
-		nameValueRequired();
-		typeAndSubmit(inputName, getMess("bean-name--valid-input"));
-		waitForTextEquals(getMessageFor(inputName), "");
-	}
-
-	@Test
-	public void ageValueRequired() {
-		openPage();
-		typeAndSubmit(inputAge, "");
-		waitForTextEquals(getMessageFor(inputAge), getMess("may-not-be-null"));
-	}
-
-	@Test
-	public void ageMinimumValue() {
-		openPage();
-		typeAndSubmit(inputAge, getMess("long-range--bellow"));
-		waitForTextEquals(getMessageFor(inputAge), getMess("bean-age--min"));
-	}
-
-	@Test
-	public void ageMaximumValue() {
-		openPage();
-		typeAndSubmit(inputAge, getMess("bean-age--max--enter"));
-		waitForTextEquals(getMessageFor(inputAge), getMess("bean-age--max"));
-	}
-
-	@Test
-	public void ageIntegerOnly() {
-		openPage();
-		typeAndSubmit(inputAge, formatMess("age-integer-only--enter", inputAge));
-		waitForTextEquals(getMessageFor(inputAge), formatMess(
-				"bean-age--integer-only", selenium.getAttribute(format(
-						"{0}/@id", inputAge))));
-	}
-
-	@Test
-	public void ageMessageDisappers() {
-		ageValueRequired();
-		typeAndSubmit(inputAge, getMess("valid-age"));
-		waitForTextEquals(getMessageFor(inputAge), "");
-	}
-
-	@Test
-	public void emailMayNotBeNullOrEmpty() {
-		openPage();
-		typeAndSubmit(inputEmail, "");
-		waitForTextEquals(getMessageFor(inputEmail),
-				getMess("may-not-be-null-or-empty"));
-	}
-
-	@Test
-	public void emailOnlySpaces() {
-		openPage();
-		typeAndSubmit(inputEmail, " ");
-		waitForTextEquals(getMessageFor(inputEmail),
-				getMess("not-well-formed-email"));
-	}
-
-	@Test
-	public void emailBad1() {
-		openPage();
-		typeAndSubmit(inputEmail, getMess("bad-email-1"));
-		waitForTextEquals(getMessageFor(inputEmail),
-				getMess("not-well-formed-email"));
-	}
-
-	@Test
-	public void emailBad2() {
-		openPage();
-		typeAndSubmit(inputEmail, getMess("bad-email-2"));
-		waitForTextEquals(getMessageFor(inputEmail),
-				getMess("not-well-formed-email"));
-	}
-
-	@Test
-	public void emailMessageDisappers() {
-		emailMayNotBeNullOrEmpty();
-		typeAndSubmit(inputEmail, getMess("well-formed-email"));
-		waitForTextEquals(getMessageFor(inputEmail), "");
+		scrollIntoView(LOC_FIELDSET_HEADER_USEF_INFO, true);
 	}
 }
