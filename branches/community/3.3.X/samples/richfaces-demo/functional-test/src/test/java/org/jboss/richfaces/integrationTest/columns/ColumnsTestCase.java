e64d9f92593b05e25a0eb04bd1a72c03cbbdfb8c
lfryc@redhat.com <lfryc@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-09-07 09:13:56 +0000

    - refactored Columns test cases to follow conventions

--- branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/columns/ColumnsTestCase.java
+++ branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/columns/ColumnsTestCase.java
@@ -1,3 +1,23 @@
+/**
+ * License Agreement.
+ *
+ *  JBoss RichFaces
+ *
+ * Copyright (C) 2009  Red Hat, Inc.
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this test suite; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 package org.jboss.richfaces.integrationTest.columns;
 
 import java.util.Arrays;
@@ -5,11 +25,12 @@ import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.Assert;
+import static org.testng.Assert.*;
 
 import org.jboss.richfaces.integrationTest.AbstractSeleniumRichfacesTestCase;
 import org.jboss.test.selenium.waiting.Condition;
 import org.jboss.test.selenium.waiting.Wait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -17,113 +38,104 @@ import org.testng.annotations.Test;
  * @version $Revision$
  */
 public class ColumnsTestCase extends AbstractSeleniumRichfacesTestCase {
+
+	private final String LOC_TABLE = getLoc("TABLE");
+	private final String LOC_TH_PREFORMATTED = getLoc("TH_PREFORMATTED");
+	private final String LOC_TD_PREFORMATTED = getLoc("TD_PREFORMATTED");
+
+	private final Pattern MSG_PATTERN_PRICE = Pattern.compile(getMsg("PATTERN_PRICE"));
+
 	/**
-	 * Opens specified page
+	 * First get map of all rows, then sort by second column and checks that
+	 * association of all rows is equal as on the start of test.
 	 */
-	public void openPage() {
-		selenium.open(format("{0}/{1}", contextPath, PAGE));
-
-		scrollIntoView(header, true);
-	}
-
-	private final String PAGE = "richfaces/columns.jsf?c=columns&tab=usage";
-	private final String table = getLoc("columns--table");
-	private final String header = table;
-	private final String cellHeaders = getLoc("columns--table-cell-header--preformatted");
-	private final String cells = getLoc("columns--table-cell--preformatted");
-	private final Pattern patternPrice = Pattern.compile(".*\\s(\\d+)\\$$");
-
 	@Test
-	public void simpleSortingTest() {
-		openPage();
-
+	public void testSimpleSorting() {
 		final int sortColumn = 2;
 
 		String[][] map = getMapOfCells();
 
-		final String contextBeforeChange = selenium.getText(table);
+		final String tableBeforeChange = selenium.getText(LOC_TABLE);
 
-		selenium.click(format(cellHeaders, sortColumn));
+		selenium.click(format(LOC_TH_PREFORMATTED, sortColumn));
 
 		sortByColumn(map, sortColumn, false);
 
 		Wait.dontFail().until(new Condition() {
 			public boolean isTrue() {
-				return !contextBeforeChange.equals(selenium.getText(table));
+				return !tableBeforeChange.equals(selenium.getText(LOC_TABLE));
 			}
 		});
 
 		String[][] newMap = getMapOfCells();
 
-		for (int i = 0; i < newMap.length; i++) {
-			Assert.assertTrue(Arrays.equals(map[i], newMap[i]));
-		}
+		assertTrue(Arrays.deepEquals(map, newMap),
+				"Association of rows isn't equal to association of rows on the start of the test");
 	}
 
+	/**
+	 * First get map of all rows, then sort by forth column in reverse order and
+	 * checks that association of all rows is equal as on the start of the test.
+	 */
 	@Test
-	public void reverseOrderSortingTest() {
-		openPage();
-
+	public void testReverseOrderSorting() {
 		final int sortColumn = 4;
 
 		String[][] map = getMapOfCells();
 
-		final String contextBeforeChange = selenium.getText(table);
+		final String contextBeforeChange = selenium.getText(LOC_TABLE);
 
-		selenium.click(format(cellHeaders, sortColumn));
+		selenium.click(format(LOC_TH_PREFORMATTED, sortColumn));
 
 		sortByColumn(map, sortColumn, true);
 
 		Wait.dontFail().until(new Condition() {
 			public boolean isTrue() {
-				return !contextBeforeChange.equals(selenium.getText(table));
+				return !contextBeforeChange.equals(selenium.getText(LOC_TABLE));
 			}
 		});
 
-		selenium.click(format(cellHeaders, sortColumn));
+		selenium.click(format(LOC_TH_PREFORMATTED, sortColumn));
 
 		Wait.dontFail().until(new Condition() {
 			public boolean isTrue() {
-				return !contextBeforeChange.equals(selenium.getText(table));
+				return !contextBeforeChange.equals(selenium.getText(LOC_TABLE));
 			}
 		});
 
 		String[][] newMap = getMapOfCells();
 
-		for (int i = 0; i < newMap.length; i++) {
-			Assert.assertTrue(Arrays.equals(map[i], newMap[i]));
-		}
+		assertTrue(Arrays.deepEquals(map, newMap),
+				"Association of rows isn't equal to association of rows on the start of the test");
 	}
 
 	private String[][] getMapOfCells() {
-		final int columns = selenium.getXpathCount(format(cells, 1, 0))
-				.intValue();
-		final int rows = selenium.getXpathCount(format(cells, 0, 1)).intValue();
+		final int columns = selenium.getXpathCount(format(LOC_TD_PREFORMATTED, 1, 0)).intValue();
+		final int rows = selenium.getXpathCount(format(LOC_TD_PREFORMATTED, 0, 1)).intValue();
 
 		String[][] map = new String[rows][columns];
 
 		for (int row = 1; row <= rows; row++) {
 			map[row - 1] = new String[columns];
 			for (int column = 1; column <= columns; column++) {
-				String cell = format(cells, row, column);
+				String cell = format(LOC_TD_PREFORMATTED, row, column);
 				map[row - 1][column - 1] = selenium.getText(cell);
 			}
 		}
 		return map;
 	}
 
-	private void sortByColumn(final String[][] map, final int column,
-			final boolean reverseOrder) {
+	private void sortByColumn(final String[][] map, final int column, final boolean reverseOrder) {
 		Arrays.sort(map, new Comparator<String[]>() {
 			public int compare(String[] o1, String[] o2) {
 				String s1 = o1[column - 1];
 				String s2 = o2[column - 1];
 
-				Matcher m1 = patternPrice.matcher(s1);
+				Matcher m1 = MSG_PATTERN_PRICE.matcher(s1);
 				m1.find();
 				int i1 = Integer.valueOf(m1.group(1));
 
-				Matcher m2 = patternPrice.matcher(s2);
+				Matcher m2 = MSG_PATTERN_PRICE.matcher(s2);
 				m2.find();
 				int i2 = Integer.valueOf(m2.group(1));
 
@@ -134,5 +146,13 @@ public class ColumnsTestCase extends AbstractSeleniumRichfacesTestCase {
 				}
 			}
 		});
+	}
+
+	@SuppressWarnings("unused")
+	@BeforeMethod
+	private void loadPage() {
+		openComponent("Columns");
+		scrollIntoView(LOC_TABLE, true);
+		selenium.allowNativeXpath("true");
 	}
 }
