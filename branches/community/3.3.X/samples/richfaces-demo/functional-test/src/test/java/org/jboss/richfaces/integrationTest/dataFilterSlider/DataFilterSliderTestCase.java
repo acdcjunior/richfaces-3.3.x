530e9be38d43be0cd82c5d7b407deda2a060baf9
lfryc@redhat.com <lfryc@redhat.com@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-09-07 11:51:03 +0000

    - refactored Data Filder Slider test cases to follow conventions

--- branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/dataFilterSlider/DataFilterSliderTestCase.java
+++ branches/community/3.3.X/samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/dataFilterSlider/DataFilterSliderTestCase.java
@@ -1,13 +1,31 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
 package org.jboss.richfaces.integrationTest.dataFilterSlider;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import junit.framework.Assert;
+import static org.testng.Assert.*;
 
 import org.jboss.richfaces.integrationTest.AbstractDataIterationTestCase;
 import org.jboss.test.selenium.waiting.Condition;
 import org.jboss.test.selenium.waiting.Wait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -15,90 +33,96 @@ import org.testng.annotations.Test;
  * @version $Revision$
  */
 public class DataFilterSliderTestCase extends AbstractDataIterationTestCase {
+
+	private final String LOC_FIELDSET_HEADER = getLoc("FIELDSET_HEADER");
+	private final String LOC_TD_MILEAGE_PREFORMATTED = getLoc("TD_MILEAGE_PREFORMATTED");
+	private final String LOC_TD_BRAND_PREFORMATTED = getLoc("TD_BRAND_PREFORMATTED");
+	private final String LOC_LINK_BRAND_PREFORMATTED = getLoc("LINK_BRAND_PREFORMATTED");
+	private final String LOC_DIV_SLIDER_HANDLE = getLoc("DIV_SLIDER_HANDLE");
+	private final String LOC_DIV_SLIDER_TRACK = getLoc("DIV_SLIDER_TRACK");
+	private final String LOC_INPUT_MAX_PRICE = getLoc("INPUT_MAX_PRICE");
+
+	private final int MSG_COUNT_MAX_ROWS = Integer.valueOf(getMsg("COUNT_MAX_ROWS"));
+	private final String MSG_CHOICES_OF_BRANDS = getMsg("CHOICES_OF_BRANDS");
+	private final String MSG_CHOICES_OF_SLIDER_PERCENTAGES = getMsg("CHOICES_OF_SLIDER_PERCENTAGES");
+	private final String MSG_CHOICES_OF_SLIDER_PERCENTAGES_FOR_INTERLEAVING = getMsg("CHOICES_OF_SLIDER_PERCENTAGES_FOR_INTERLEAVING");
+	private final String MSG_INPUT_SIMPLE_SLIDER_PERCENTAGE = getMsg("INPUT_SIMPLE_SLIDER_PERCENTAGE");
+
 	/**
-	 * Opens specified page
+	 * Click on the specified brand and checks that all rows contains only
+	 * specified brand (column Make)
 	 */
-	public void openPage() {
-		selenium.open(format("{0}/{1}", contextPath, PAGE));
-
-		scrollIntoView(header, true);
+	@Test
+	public void testSimpleBrand() {
+		testBrand(format(MSG_CHOICES_OF_BRANDS, 0));
 	}
 
-	private final String PAGE = "richfaces/dataFilterSlider.jsf?c=dataFilterSlider&tab=usage";
-	private final String header = getLoc("data-filter-slider--header");
-	private final String cellsMileage = getLoc("data-filter-slider--table-cells--mileage-preformatted");
-	private final String cellsBrand = getLoc("data-filter-slider--table-cells--brand-preformatted");
-	private final int maxRowCount = Integer
-			.valueOf(getMess("data-filter-slider--count--max-rows-in-table"));
-	private final String brands = getMess("data-filter-slider--choices--brands");
-	private final String percentages = getMess("data-filter-slider--choices--slider-percentages");
-	private final String percentagesInterleaving = getMess("data-filter-slider--choices--slider-percentages-interleaving");
-	private final String percentageSimple = getMess("data-filter-slider--simple-slider-percentage");
-
+	/**
+	 * Use slider to change input price and checks that all prices in table is
+	 * lowe than price selected by slider and given in text-input next to the
+	 * slider
+	 */
 	@Test
-	public void simpleBrandTest() {
-		openPage();
-		testBrand(format(brands, 0));
+	public void testSimpleSlider() {
+		testSlider(MSG_INPUT_SIMPLE_SLIDER_PERCENTAGE);
 	}
 
+	/**
+	 * Use several values of prices to use with slider and checks that in each
+	 * step is contained all values defined in all steps with lesser selected
+	 * value of price
+	 */
 	@Test
-	public void simpleSliderTest() {
-		openPage();
-		testSlider(percentageSimple);
-	}
-
-	@Test
-	public void complexSliderTest() {
-		openPage();
-
-		List<Integer> lowMileages = testSlider(format(percentages, 0));
-		List<Integer> highMileages = testSlider(format(percentages, 1));
-		List<Integer> lowestMileages = testSlider(format(percentages, 2));
-		List<Integer> allMileages = testSlider(format(percentages, 3));
+	public void testComplexSlider() {
+		List<Integer> lowMileages = testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES, 0));
+		List<Integer> highMileages = testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES, 1));
+		List<Integer> lowestMileages = testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES, 2));
+		List<Integer> allMileages = testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES, 3));
 
 		// check that each state of table contains all mileages in states with
 		// lesser specified max mileage (don't try to check tables that have
 		// maxRowCount and more rows, because tables are constrained by
 		// maxRowCount per table and so don't have to include all lesser tables)
-		Assert.assertTrue(allMileages.size() >= maxRowCount
-				|| allMileages.containsAll(highMileages));
-		Assert.assertTrue(highMileages.size() >= maxRowCount
-				|| highMileages.containsAll(lowMileages));
-		Assert.assertTrue(lowMileages.size() >= maxRowCount
-				|| lowMileages.containsAll(lowestMileages));
+		assertTrue(allMileages.size() >= MSG_COUNT_MAX_ROWS || allMileages.containsAll(highMileages));
+		assertTrue(highMileages.size() >= MSG_COUNT_MAX_ROWS || highMileages.containsAll(lowMileages));
+		assertTrue(lowMileages.size() >= MSG_COUNT_MAX_ROWS || lowMileages.containsAll(lowestMileages));
 		// check that there is at least one mileage greater than slider minimum
-		Assert.assertFalse(lowestMileages.isEmpty());
+		assertFalse(lowestMileages.isEmpty());
 	}
 
+	/**
+	 * Test several predefined value of selected brand and selected price to
+	 * test influencing and interleaving of 'by slider' and 'by brand' selection
+	 * models. First use slider to select value and test all prices in table are
+	 * lesser than selected price, next click on the brand (column Make) and
+	 * checks that only given brand is in table. Then repeat this process once
+	 * with another input data.
+	 */
 	@Test
-	public void interleavingMethodsTest() {
-		openPage();
-		testSlider(format(percentagesInterleaving, 0));
-		testBrand(format(brands, 0));
-		testSlider(format(percentagesInterleaving, 1));
-		testBrand(format(brands, 1));
-		testSlider(format(percentagesInterleaving, 2));
+	public void testMethodInterleaving() {
+		testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES_FOR_INTERLEAVING, 0));
+		testBrand(format(MSG_CHOICES_OF_BRANDS, 0));
+		testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES_FOR_INTERLEAVING, 1));
+		testBrand(format(MSG_CHOICES_OF_BRANDS, 1));
+		testSlider(format(MSG_CHOICES_OF_SLIDER_PERCENTAGES_FOR_INTERLEAVING, 2));
 	}
 
 	private void testBrand(String brand) {
-		final String table = getLoc("data-table-common--table");
-		final String linkBrandPreformatted = getLoc("data-filter-slider--link--brand-preformatted");
+		final String locLinkBrand = format(LOC_LINK_BRAND_PREFORMATTED, brand);
 
-		String linkBrand = format(linkBrandPreformatted, brand);
-
-		final String tableText = selenium.getText(table);
-		selenium.click(linkBrand);
+		final String tableText = selenium.getText(LOC_TABLE_COMMON);
+		selenium.click(locLinkBrand);
 
 		Wait.dontFail().timeout(5000).until(new Condition() {
 			public boolean isTrue() {
-				return !tableText.equals(selenium.getText(table));
+				return !tableText.equals(selenium.getText(LOC_TABLE_COMMON));
 			}
 		});
 
-		for (int row = 1; row <= selenium.getXpathCount(format(cellsBrand, 0))
-				.intValue(); row++) {
-			String rowBrand = selenium.getText(format(cellsBrand, row));
-			Assert.assertEquals(brand, rowBrand);
+		for (int row = 1; row <= selenium.getXpathCount(format(LOC_TD_BRAND_PREFORMATTED, 0)).intValue(); row++) {
+			String rowBrand = selenium.getText(format(LOC_TD_BRAND_PREFORMATTED, row));
+
+			assertEquals(rowBrand, brand, "In all rows must be the defined brand");
 		}
 	}
 
@@ -110,40 +134,47 @@ public class DataFilterSliderTestCase extends AbstractDataIterationTestCase {
 
 	private List<Integer> checkAllMileagesMaxAndReturnItsList(int maxMileage) {
 		List<Integer> list = new LinkedList<Integer>();
-		int rowCount = selenium.getXpathCount(format(cellsMileage, 0)).intValue();
+
+		int rowCount = selenium.getXpathCount(format(LOC_TD_MILEAGE_PREFORMATTED, 0)).intValue();
+
 		for (int row = 1; row <= rowCount; row++) {
-			int rowMileage = Double.valueOf(
-					selenium.getText(format(cellsMileage, row))).intValue();
-			Assert.assertTrue(rowMileage <= maxMileage);
+			int rowMileage = Double.valueOf(selenium.getText(format(LOC_TD_MILEAGE_PREFORMATTED, row))).intValue();
+
+			assertTrue(rowMileage <= maxMileage, format(
+					"All mileages in table must be lesser than selected value ({0}), but '{1}' isn't", maxMileage,
+					rowMileage));
+
 			list.add(rowMileage);
 		}
 		return list;
 	}
 
 	private int getCurrentMileageFromInput() {
-		final String inputPrice = getLoc("data-filter-slider--input--price");
-		return Integer.valueOf(selenium.getValue(inputPrice));
+		return Integer.valueOf(selenium.getValue(LOC_INPUT_MAX_PRICE));
 	}
 
 	private void clickSliderAtPercent(double percent) {
-		final String handle = getLoc("data-filter-slider--handle");
-		final String track = getLoc("data-filter-slider--track");
+		final Number handlePosition = selenium.getElementPositionLeft(LOC_DIV_SLIDER_HANDLE);
 
-		final Number handlePosition = selenium.getElementPositionLeft(handle);
+		int width = selenium.getElementWidth(LOC_DIV_SLIDER_TRACK).intValue();
+		int height = selenium.getElementHeight(LOC_DIV_SLIDER_TRACK).intValue();
+		String coords = format("{0,number,integer},{1}", width * percent / 100, height / 2);
 
-		int width = selenium.getElementWidth(track).intValue();
-		int height = selenium.getElementHeight(track).intValue();
-		String coords = format("{0,number,integer},{1}", width * percent / 100,
-				height / 2);
+		selenium.mouseDownAt(LOC_DIV_SLIDER_TRACK, coords);
+		selenium.mouseUpAt(LOC_DIV_SLIDER_TRACK, coords);
 
-		selenium.mouseDownAt(track, coords);
-		selenium.mouseUpAt(track, coords);
-
-		Wait.until(new Condition() {
+		Wait.failWith("Slider never changes left position").until(new Condition() {
 			public boolean isTrue() {
-				return !handlePosition.equals(selenium
-						.getElementPositionLeft(handle));
+				return !handlePosition.equals(selenium.getElementPositionLeft(LOC_DIV_SLIDER_HANDLE));
 			}
 		});
+	}
+
+	@SuppressWarnings("unused")
+	@BeforeMethod
+	private void loadPage() {
+		openComponent("Data Filter Slider");
+		scrollIntoView(LOC_FIELDSET_HEADER, true);
+		selenium.allowNativeXpath("true");
 	}
 }
