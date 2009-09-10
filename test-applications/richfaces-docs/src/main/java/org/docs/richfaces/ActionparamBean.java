45aa4ac5a665aa6504a2e39b0ec7660a219cd192
artdaw <artdaw@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2009-09-10 11:20:36 +0000

    https://jira.jboss.org/jira/browse/RF-7275 - <a4j:actionparam> sample is added

---
+++ test-applications/richfaces-docs/src/main/java/org/docs/richfaces/ActionparamBean.java
@@ -0,0 +1,14 @@
+package org.docs.richfaces;
+
+
+public class ActionparamBean {
+    private String name = "John";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
