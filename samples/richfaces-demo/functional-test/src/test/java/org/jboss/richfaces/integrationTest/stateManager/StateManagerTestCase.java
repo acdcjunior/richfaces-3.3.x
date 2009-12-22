0ca42fbfbc704ab5e2a98b18c353919da2702d88
ppitonak <ppitonak@redhat.com>
2009-12-22 14:09:09 +0000

    https://jira.jboss.org/jira/browse/RFPL-300
    * test refactored -- added skipping tests if running with JSF 1.2

--- samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/stateManager/StateManagerTestCase.java
+++ samples/richfaces-demo/functional-test/src/test/java/org/jboss/richfaces/integrationTest/stateManager/StateManagerTestCase.java
@@ -24,18 +24,19 @@ package org.jboss.richfaces.integrationTest.stateManager;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotSame;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
 import org.jboss.richfaces.integrationTest.AbstractSeleniumRichfacesTestCase;
 import org.jboss.test.selenium.waiting.Condition;
 import org.jboss.test.selenium.waiting.Wait;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
- * A test case for State Manager API. It verifies https://jira.jboss.org/jira/browse/RF-8204. To run this
- * test, demo has to run on top of JSF 2 and server-side state serialization has to be switched on.
+ * A test case for State Manager API. It verifies https://jira.jboss.org/jira/browse/RF-8204. To run this test, demo has
+ * to run on top of JSF 2 and server-side state serialization has to be switched on. If the test suite is launched with
+ * JSF 1.2, all tests in this class will be skipped.
  * 
  * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
  * @version $Revision$
@@ -45,42 +46,42 @@ public class StateManagerTestCase extends AbstractSeleniumRichfacesTestCase {
     private final String LOC_HIDDEN_INPUT_PREFORMATTED = getLoc("HIDDEN_INPUT_PREFORMATTED");
     private final String LOC_LINK = getLoc("LINK");
 
-     /**
-      * Tests behavior of the application after Ajax request. It clicks on the link in the form on the page. There
-      * are three forms on the page, each of them has to contain the same value which cannot change after Ajax request. 
-      */
-     @Test
-     public void testAjaxRequest() {
-     String [] oldValues = new String[3];
-     getValuesAndVerify(oldValues);
-            
-        
-     selenium.click(LOC_LINK);
-     Wait.failWith("The form did not change").until(new Condition() {
-     public boolean isTrue() {
-     return selenium.getText(LOC_LINK).equals("(To login)");
-     }
-     });
-            
-     String [] newValues = new String[3];
-     getValuesAndVerify(newValues);
-            
-     assertEquals(oldValues[0], newValues[0],
-     "The value attribute in all forms should not change after an Ajax request.");
-     }
+    /**
+     * Tests behavior of the application after Ajax request. It clicks on the link in the form on the page. There are
+     * three forms on the page, each of them has to contain the same value which cannot change after Ajax request.
+     */
+    @Test(dependsOnMethods = { "testJsfVersion" })
+    public void testAjaxRequest() {
+        String[] oldValues = new String[3];
+        getValuesAndVerify(oldValues);
+
+        selenium.click(LOC_LINK);
+        Wait.failWith("The form did not change").until(new Condition() {
+            public boolean isTrue() {
+                return selenium.getText(LOC_LINK).equals("(To login)");
+            }
+        });
+
+        String[] newValues = new String[3];
+        getValuesAndVerify(newValues);
+
+        assertEquals(oldValues[0], newValues[0],
+            "The value attribute in all forms should not change after an Ajax request.");
+    }
 
     /**
-     * Tests behavior of the application after non-Ajax request. It reloads the page which will cause a non-Ajax request.
-     * There are three forms on the page, each of them has to contain the same value which has to change after non-Ajax request.
+     * Tests behavior of the application after non-Ajax request. It reloads the page which will cause a non-Ajax
+     * request. There are three forms on the page, each of them has to contain the same value which has to change after
+     * non-Ajax request.
      */
-    @Test
+    @Test(dependsOnMethods = { "testJsfVersion" })
     public void testNonAjaxRequest() {
         String[] oldValues = new String[3];
         getValuesAndVerify(oldValues);
 
         selenium.refresh();
         waitFor(4000);
-        
+
         String[] newValues = new String[3];
         getValuesAndVerify(newValues);
 
@@ -88,6 +89,11 @@ public class StateManagerTestCase extends AbstractSeleniumRichfacesTestCase {
             "The value attribute in all forms should change after a non-Ajax request.");
     }
 
+    /**
+     * Gets the "value" attribute in three form on the page and verifies that all are the same.
+     * 
+     * @param values
+     */
     private void getValuesAndVerify(String[] values) {
         for (int i = 0; i < 3; i++) {
             values[i] = selenium.getAttribute(format(LOC_HIDDEN_INPUT_PREFORMATTED, i));
@@ -97,6 +103,21 @@ public class StateManagerTestCase extends AbstractSeleniumRichfacesTestCase {
             "The attribute value in the first form should be the same as one in the second form.");
         assertEquals(values[1], values[2],
             "The attribute value in the second form should be the same as one in the third form.");
+    }
+
+    /**
+     * Tests that the test suite is running with JSF2. If test suite is launched with JSF 1.x, all tests in this class
+     * are skipped.
+     * 
+     * @param jsfMajorVersion
+     *            major version of JSF, 1 or 2
+     */
+    @Test
+    @Parameters( { "jsf.majorVersion" })
+    public void testJsfVersion(String jsfMajorVersion) {
+        if (!"2".equals(jsfMajorVersion)) {
+            throw new SkipException("Test should be launched only if running on top of JSF 2.");
+        }
     }
 
     /**
