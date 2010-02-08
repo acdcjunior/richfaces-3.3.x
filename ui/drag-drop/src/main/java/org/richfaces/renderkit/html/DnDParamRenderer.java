702cb26eb47aac31bb91e7c6813a771937d8607e
amarkhel <amarkhel@46473bb9-a92a-0410-8f14-e4f2f95f0492>
2010-02-08 20:30:00 +0000

    Fix https://jira.jboss.org/jira/browse/RF-8347

---
+++ ui/drag-drop/src/main/java/org/richfaces/renderkit/html/DnDParamRenderer.java
@@ -0,0 +1,13 @@
+package org.richfaces.renderkit.html;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.render.Renderer;
+
+public class DnDParamRenderer extends Renderer{
+
+	public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
+	}
+}
